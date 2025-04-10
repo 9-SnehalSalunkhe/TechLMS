988a321d30bca0d3c820b01c300f0287e38cfb97
unknown <snehalsalunkhe8989@gmail.com>
2025-04-10 14:04:50 +0530

    Signup and Login form completed

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -15,16 +15,40 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+import { useEffect, useState } from "react";
 
-const Login =() => {
+const Login = () => {
+  const [signupInput, setSignupInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [loginInput, setLoginInput] = useState({ email: "", password: "" });
+
+  const changeInputHandler = (e, type) => {
+    const { name, value } = e.target;
+
+    if (type === "signup") {
+      setSignupInput((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    } else if (type === "login") {
+      setLoginInput((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
+
   return (
    <div className = "flex item-center w-full justify-center">
      <Tabs defaultValue="account" className="w-[400px]">
       <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="Signup">Signup</TabsTrigger>
-        <TabsTrigger value="Login">Login</TabsTrigger>
+        <TabsTrigger value="signup">Signup</TabsTrigger>
+        <TabsTrigger value="login">Login</TabsTrigger>
       </TabsList>
-      <TabsContent value="Signup">
+      <TabsContent value="signup">
         <Card>
           <CardHeader>
             <CardTitle>Signup Here</CardTitle>
@@ -35,15 +59,24 @@ const Login =() => {
           <CardContent className="space-y-2">
             <div className="space-y-1">
               <Label htmlFor="name">Name</Label>
-              <Input type ="text" placeholder="eg.John" required ="true"/>
+              <Input type ="text" placeholder="eg.John" required ="true"
+              name="name"
+              value={signupInput.name}
+              onChange={(e) => changeInputHandler(e, "signup")}/>
             </div>
             <div className="space-y-1">
               <Label htmlFor="username">Email</Label>
-              <Input type ="email" placeholder="eg.John123@gmail.com" required ="true"/>
+              <Input type ="email" placeholder="eg.John123@gmail.com" required ="true"
+              name="email"
+              value={signupInput.email}
+              onChange={(e) => changeInputHandler(e, "signup")}/>
             </div>
             <div className="space-y-1">
               <Label htmlFor="username">Password</Label>
-              <Input type ="email" placeholder="eg.John123" required ="true"/>
+              <Input type ="email" placeholder="eg.John123" required ="true"
+               name="password"
+               value={signupInput.password}
+               onChange={(e) => changeInputHandler(e, "signup")}/>
             </div>
           </CardContent>
           <CardFooter>
@@ -51,7 +84,7 @@ const Login =() => {
           </CardFooter>
         </Card>
       </TabsContent>
-      <TabsContent value="Login">
+      <TabsContent value="login">
         <Card>
           <CardHeader>
             <CardTitle>login Here</CardTitle>
@@ -62,11 +95,17 @@ const Login =() => {
           <CardContent className="space-y-2">
             <div className="space-y-1">
               <Label htmlFor="current">Email</Label>
-              <Input type="email" placeholder="eg.John123@gmail.com" required="true"/>
+              <Input type="email" placeholder="eg.John123@gmail.com" required="true"
+              name="email"
+              value={loginInput.email}
+              onChange={(e) => changeInputHandler(e, "login")}/>
             </div>
             <div className="space-y-1">
               <Label htmlFor="new">Password</Label>
-              <Input type="password" placeholder="eg.John123" required="true" />
+              <Input type="password" placeholder="eg.John123" required="true"
+               name="password"
+               value={loginInput.password}
+               onChange={(e) => changeInputHandler(e, "login")} />
             </div>
           </CardContent>
           <CardFooter>
